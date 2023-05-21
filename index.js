3c22fd108e6264fa6c679bba9c6750d1264559fb
raghib122 <125207689+raghib122@users.noreply.github.com>
2023-05-22 00:45:51 +0530

    Update index.js
    Updated with weather changeable background

--- index.js
+++ index.js
@@ -20,24 +20,45 @@ function getResults(query) {
     .then(displayResults);
 }
 
-function displayResults(weather) {
-  console.log(weather);
-  let city = document.querySelector(".location .city");
-  city.innerText = `${weather.name}, ${weather.sys.country}`;
 
-  let now = new Date();
-  let date = document.querySelector(".location .date");
-  date.innerText = dateBuilder(now);
-
-  let temp = document.querySelector(".current .temp");
-  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
-
-  let weather_el = document.querySelector(".current .weather");
-  weather_el.innerText = weather.weather[0].main;
-
-  let hilow = document.querySelector(".hi-low");
-  hilow.innerText = `${weather.main.temp_min}°C / ${weather.main.temp_max}°C`;
-}
+  function displayResults(weather) {
+    let city = document.querySelector(".location .city");
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+  
+    let now = new Date();
+    let date = document.querySelector(".location .date");
+    date.innerText = dateBuilder(now);
+  
+    let temp = document.querySelector(".current .temp");
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+  
+    let weather_el = document.querySelector(".current .weather");
+    weather_el.innerText = weather.weather[0].main;
+  
+    let hilow = document.querySelector(".hi-low");
+    hilow.innerText = `${weather.main.temp_min}°C / ${weather.main.temp_max}°C`;
+  
+    let body = document.querySelector("body");
+  
+    if (weather.weather[0].main === "Sunny") {
+      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1605371893234-db5e7b01aad2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')";
+    } else if (weather.weather[0].main === "Clouds") {
+      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1532178910-7815d6919875?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80')";
+    } else if (weather.weather[0].main === "Haze") {
+        body.style.backgroundImage = "url('https://images.unsplash.com/photo-1527666466760-f6a7a2b31176?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80')";
+      }
+      else if (weather.weather[0].main === "Clear") {
+        body.style.backgroundImage = "url('https://images.unsplash.com/photo-1601297183305-6df142704ea2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80')";
+      }
+      
+    else if (weather.weather[0].main === "Rain") {
+      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1429552054921-018e433d7d34?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1176&q=80')";
+    } else if (weather.weather[0].main === "Snow") {
+      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1578410290083-b6ed52942332?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80')";
+    } else {
+      body.style.backgroundImage = "url('https://images.unsplash.com/photo-1526943604017-955071a1fb3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')";
+    }
+  }
 
 function dateBuilder(d) {
   let months = [
